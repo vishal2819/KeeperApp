8a31d08057d42a047f399e00b67ef15a1044b59c
vishalEskill GitHub <vishalramteke99@gmail.com>
2023-02-15 16:20:36 +0530

    First commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { Highlight } from "@material-ui/icons";
+function Header() {
+  return (
+    <header><h1><Highlight />Keeper</h1>
+    </header>
+  );
+}
+
+export default Header;
